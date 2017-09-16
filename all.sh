f46a1b6932c9b7bef85ddf34db938ca8e93ed75c
Fang Dehui <javaeecoding@gmail.com>
2017-09-16 16:27:54 +0900

    init

---
+++ all.sh
@@ -0,0 +1,34 @@
+#get proxy list
+export http_proxy=
+rm list_proxy
+rm list_may_ok
+rm list_ok
+
+groovy GetProxy.gv | egrep -o '[0-9]+(?:\.[0-9]+){3}:[0-9]+' > list_proxy
+
+expect=`cat expect.txt`
+
+#get ok proxy ( response in 1 second)
+while read p; do
+   export http_proxy=http://$p; 
+   resp=$(curl -m 3 music.163.com/song?id=362396); 
+   if [[ (! $resp =~ .*\<\/html\> ) || ( $resp =~ .*由于版权保.* ) || (! $resp =~ .*日光倾城.* )  ]]; then 
+       echo "$p  NG" ;  
+   else ( 
+       echo "$p  OK"; 
+       echo "$p" >> list_ok;
+
+       echo $resp > $p; 
+    )
+   fi  
+done <list_proxy
+
+#format it to pac
+echo "-------May OK------"
+cat list_may_ok | sed -e 's/^/"PROXY_/g' | sed -e 's/[ ]*$/;"+/g' | sed -e 's/  */:/g' | sed 's/_/ /g'
+
+echo "=======OK========"
+cat list_ok | sed -e 's/^/"PROXY_/g' | sed -e 's/[ ]*$/;"+/g' | sed -e 's/  */:/g' | sed 's/_/ /g'
+  
+#clear
+export http_proxy=
---
+++ pxy.pac
@@ -0,0 +1,19 @@
+function FindProxyForURL(url, host) {
+  
+if (url.indexOf(".mp3")>1 || url.indexOf(".mp4")>1 || url.indexOf(".flv")>1)
+  return "DIRECT";
+  
+var pxyList = (
+  
+  
+REPLACE_ME
+  
+  "").replace(/;$/,"").split(";");
+var r=Math.floor(Math.random() * pxyList.length);
+  
+if (host.indexOf("103.211.228.")>-1 || host == "music.163.com" || url.indexOf('music.126.net')>-1 ) 
+  return pxyList[r]+";";
+
+return "DIRECT";
+}
+
